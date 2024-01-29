9c605d54e952f07bfa6e8b37fb100807a64aa733
= <=>
2024-01-29 07:56:24 -0700

    fixing spelling for FuZzionZ

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P5ZNFPUZ.mjs";import"./chunk-375QZNE3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PPXKJQUZ.mjs";import"./chunk-375QZNE3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -24,7 +24,7 @@ export class LeaderboardComponent implements OnInit {
   selectedFilter: string = ''; // Add this property
   filteredPlayers: Player[] = [];
   displayedColumns: string[] = ['place', 'name', 'wins', 'losses', 'winRate'];
-  pageTitle: string = 'FuzZionZ Seasons Leaderboard';
+  pageTitle: string = 'FuZzionZ Seasons Leaderboard';
   champions: Player[] = [];
 
   ngOnInit(): void {
