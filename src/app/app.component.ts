5587dc031fc417d109e301abed911f32af61ee5f
= <=>
2024-01-29 00:27:19 -0700

    updated them and trophy color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'leaderboard';
+  @HostBinding('class') componentCssClass: string;
+
+  constructor() {
+    // Add your dark mode class here
+    this.componentCssClass = 'dark-theme';
+  }
 }
